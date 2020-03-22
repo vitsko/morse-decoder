fcb19e463667dca00622599eeeea4b75f72cc919
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2020-03-22 18:18:01 +0400

    feat: add solution task 'Morse code decoder'

--- src/index.js
+++ src/index.js
@@ -1,44 +1,75 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
+function getLetter(expr) {
+    if (expr[0] == '*') {
+        return ' ';
+    }
+
+    let regexpPosition = 0;//expr.search('/1*/');
+
+    for (; regexpPosition < expr.length && expr[regexpPosition] != '1'; regexpPosition++);
+
+    expr = expr.substr(regexpPosition, expr.length - regexpPosition);
+
+    let result = '';
+
+    for (let i = 0; i < expr.length; i += 2) {
+        if (expr.substr(i, 2) == '10') {
+            result = result.concat('.');
+        } else if (expr.substr(i, 2) == '11') {
+            result = result.concat('-');
+        }
+    }
+
+    return MORSE_TABLE[result];
+}
+
 function decode(expr) {
-    // write your solution here
+    result = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        let sub = expr.substr(i, 10);
+        result = result.concat(getLetter(sub));
+    }
+
+    return result;
 }
 
 module.exports = {
